3a818e8c76c9d5acc834cd959420900ee172f02d
Nikwotton <nwotton@cmtelematics.com>
2022-12-01 00:29:58 -0500

    added template

---
+++ src/day00_template/Main.kt
@@ -0,0 +1,28 @@
+package day00_template
+
+import java.io.File
+
+const val workingDir = "src/day00_template"
+
+fun main() {
+    val sample = File("$workingDir/sample.txt")
+    val input1 = File("$workingDir/input_1.txt")
+    println("Step 1a: ${runStep1(sample)}")
+    println("Step 1b: ${runStep1(input1)}")
+    println("Step 2a: ${runStep2(sample)}")
+    println("Step 2b: ${runStep2(input1)}")
+}
+
+fun runStep1(input: File): String {
+    input.readLines().forEach {
+        TODO()
+    }
+    return TODO()
+}
+
+fun runStep2(input: File): String {
+    input.readLines().forEach {
+        TODO()
+    }
+    return TODO()
+}
