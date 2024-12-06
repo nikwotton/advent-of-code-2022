d18b76434a943064bd8fadf5805ded78f07b6b2d
Nikolai Wotton <wottonn@gmail.com>
2024-12-06 00:27:17 -0500

    day 6

--- src/day06/Main.kt
+++ src/day06/Main.kt
@@ -1,13 +1,14 @@
 package day06
 
+import day06.Direction.*
 import java.io.File
 
 val workingDir = "src/${object {}.javaClass.`package`.name}"
 
 fun main() {
     val sample = File("$workingDir/sample.txt")
-    val step1SampleExpected = "TODO(step1)"
-    val step2SampleExpected = "TODO(step2)"
+    val step1SampleExpected = "41"
+    val step2SampleExpected = "6"
     val input1 = File("$workingDir/input_1.txt")
     val step1Sample = runStep1(sample)
     require(step1Sample == step1SampleExpected) { "Failed sample in step 1, got $step1Sample, instead of $step1SampleExpected" }
@@ -18,15 +19,89 @@ fun main() {
 }
 
 fun runStep1(input: File): String {
-    input.readLines().forEach {
-        TODO()
+    val map = input.readLines().map { it.toCharArray() }
+    val startingY = map.indexOfFirst { it.contains('^') }
+    val startingX = map[startingY].indexOf('^')
+    val seenPoints = mutableSetOf<Position>()
+    var currentPosition = Position(startingX, startingY)
+    var currentDirection = UP
+    while (currentPosition.x >= 0 && currentPosition.y >= 0 && currentPosition.x < map[0].size && currentPosition.y < map.size) {
+        seenPoints.add(currentPosition)
+        val nextPosition = when (currentDirection) {
+            UP -> currentPosition.copy(y = currentPosition.y - 1)
+            DOWN -> currentPosition.copy(y = currentPosition.y + 1)
+            LEFT -> currentPosition.copy(x = currentPosition.x - 1)
+            RIGHT -> currentPosition.copy(x = currentPosition.x + 1)
+        }
+        if (nextPosition.x < 0 || nextPosition.y < 0 || nextPosition.x >= map[0].size || nextPosition.y >= map.size) {
+            break
+        }
+        val nextThing = map[nextPosition.y][nextPosition.x]
+        if (nextThing == '#') {
+            currentDirection = when (currentDirection) {
+                UP -> RIGHT
+                DOWN -> LEFT
+                LEFT -> UP
+                RIGHT -> DOWN
+            }
+        } else {
+            currentPosition = nextPosition
+        }
     }
-    return TODO()
+    return seenPoints.size.toString()
+}
+
+data class Position(val x: Int, val y: Int)
+enum class Direction {
+    UP, DOWN, LEFT, RIGHT
+}
+
+fun canExit(map: List<List<Char>>): Boolean {
+    val maxLoop = 6000
+    val startingY = map.indexOfFirst { it.contains('^') }
+    val startingX = map[startingY].indexOf('^')
+    val seenPoints = mutableSetOf<Position>()
+    var currentPosition = Position(startingX, startingY)
+    var currentDirection = UP
+    var i = 0
+    while (currentPosition.x >= 0 && currentPosition.y >= 0 && currentPosition.x < map[0].size && currentPosition.y < map.size) {
+        if (i++ > maxLoop) {
+            return false
+        }
+        seenPoints.add(currentPosition)
+        val nextPosition = when (currentDirection) {
+            UP -> currentPosition.copy(y = currentPosition.y - 1)
+            DOWN -> currentPosition.copy(y = currentPosition.y + 1)
+            LEFT -> currentPosition.copy(x = currentPosition.x - 1)
+            RIGHT -> currentPosition.copy(x = currentPosition.x + 1)
+        }
+        if (nextPosition.x < 0 || nextPosition.y < 0 || nextPosition.x >= map[0].size || nextPosition.y >= map.size) {
+            return true
+        }
+        val nextThing = map[nextPosition.y][nextPosition.x]
+        if (nextThing == '#') {
+            currentDirection = when (currentDirection) {
+                UP -> RIGHT
+                DOWN -> LEFT
+                LEFT -> UP
+                RIGHT -> DOWN
+            }
+        } else {
+            currentPosition = nextPosition
+        }
+    }
+    TODO()
 }
 
 fun runStep2(input: File): String {
-    input.readLines().forEach {
-        TODO()
-    }
-    return TODO()
+    val map = input.readLines().map { it.toCharArray() }
+    return map.mapIndexed { y, s ->
+        s.mapIndexed { x, c ->
+            !canExit(map.mapIndexed { innerY, s ->
+                s.mapIndexed { innerX, c ->
+                    if (innerY == y && innerX == x && c == '.') '#' else c
+                }
+            })
+        }
+    }.flatten().count { it }.toString()
 }
