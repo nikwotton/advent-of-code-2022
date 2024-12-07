02349bd5d119714f7f87aedf30821daee66e9fea
Nikolai Wotton <wottonn@gmail.com>
2024-12-07 00:12:08 -0500

    day 7

--- src/day07/Main.kt
+++ src/day07/Main.kt
@@ -1,13 +1,14 @@
 package day07
 
 import java.io.File
+import java.math.BigInteger
 
 val workingDir = "src/${object {}.javaClass.`package`.name}"
 
 fun main() {
     val sample = File("$workingDir/sample.txt")
-    val step1SampleExpected = "TODO(step1)"
-    val step2SampleExpected = "TODO(step2)"
+    val step1SampleExpected = "3749"
+    val step2SampleExpected = "11387"
     val input1 = File("$workingDir/input_1.txt")
     val step1Sample = runStep1(sample)
     require(step1Sample == step1SampleExpected) { "Failed sample in step 1, got $step1Sample, instead of $step1SampleExpected" }
@@ -17,16 +18,34 @@ fun main() {
     println("Step 2 answer: ${runStep2(input1)}")
 }
 
-fun runStep1(input: File): String {
-    input.readLines().forEach {
-        TODO()
-    }
-    return TODO()
+fun runStep1(input: File): String = input.readLines().map { line ->
+    val (outputString, inputsString) = line.split(": ")
+    val output = outputString.toBigInteger()
+    val inputs = inputsString.split(" ").map { it.toBigInteger() }
+    if (testRun(inputs, output)) output else "0".toBigInteger()
+}.fold("0".toBigInteger(), { acc, a -> acc.add(a) }).toString()
+
+fun testRun(inputs: List<BigInteger>, output: BigInteger): Boolean = if (inputs.size == 1) {
+    inputs[0] == output
+} else {
+    testRun(arrayListOf(inputs[0] + inputs[1]) + inputs.drop(2), output) ||
+            testRun(arrayListOf(inputs[0] * inputs[1]) + inputs.drop(2), output)
 }
 
-fun runStep2(input: File): String {
-    input.readLines().forEach {
-        TODO()
-    }
-    return TODO()
+fun runStep2(input: File): String = input.readLines().map { line ->
+    val (outputString, inputsString) = line.split(": ")
+    val output = outputString.toBigInteger()
+    val inputs = inputsString.split(" ").map { it.toBigInteger() }
+    if (testRun2(inputs, output)) output else "0".toBigInteger()
+}.fold("0".toBigInteger(), { acc, a -> acc.add(a) }).toString()
+
+fun testRun2(inputs: List<BigInteger>, output: BigInteger): Boolean = if (inputs.size == 1) {
+    inputs[0] == output
+} else {
+    testRun2(arrayListOf(inputs[0] + inputs[1]) + inputs.drop(2), output) ||
+            testRun2(arrayListOf(inputs[0] * inputs[1]) + inputs.drop(2), output) ||
+            testRun2(
+                arrayListOf((inputs[0].toString() + inputs[1].toString()).toBigInteger()) + inputs.drop(2),
+                output
+            )
 }
