f5540a3abdeedadba83d1084e6cbbaad6e908064
Nikolai Wotton <wottonn@gmail.com>
2024-12-09 00:48:04 -0500

    day 9

--- src/day09/Main.kt
+++ src/day09/Main.kt
@@ -1,13 +1,14 @@
 package day09
 
 import java.io.File
+import java.math.BigInteger
 
 val workingDir = "src/${object {}.javaClass.`package`.name}"
 
 fun main() {
     val sample = File("$workingDir/sample.txt")
-    val step1SampleExpected = "TODO(step1)"
-    val step2SampleExpected = "TODO(step2)"
+    val step1SampleExpected = "1928"
+    val step2SampleExpected = "2858"
     val input1 = File("$workingDir/input_1.txt")
     val step1Sample = runStep1(sample)
     require(step1Sample == step1SampleExpected) { "Failed sample in step 1, got $step1Sample, instead of $step1SampleExpected" }
@@ -18,15 +19,87 @@ fun main() {
 }
 
 fun runStep1(input: File): String {
-    input.readLines().forEach {
-        TODO()
+    val startingArray = mutableListOf<Int>()
+    var isBlock = true
+    var index = 0
+    input.readLines().first().forEach {
+        if (isBlock) {
+            (0 until it.digitToInt()).forEach { startingArray.add(index) }
+            isBlock = false
+        } else {
+            (0 until it.digitToInt()).forEach { startingArray.add(-1) }
+            isBlock = true
+            index++
+        }
     }
-    return TODO()
+    while (startingArray.last() == -1) {
+        startingArray.removeLast()
+    }
+    while (startingArray.contains(-1)) {
+        val last = startingArray.removeLast()
+        val i = startingArray.indexOfFirst { it == -1 }
+        startingArray.removeAt(i)
+        startingArray.add(i, last)
+        while (startingArray.last() == -1) {
+            startingArray.removeLast()
+        }
+    }
+    return startingArray.mapIndexed { i, c -> i.toBigInteger() * c.toBigInteger() }
+        .fold(BigInteger.ZERO) { acc, i ->
+            acc.plus(i)
+        }.toString()
 }
 
 fun runStep2(input: File): String {
-    input.readLines().forEach {
-        TODO()
+    val startingArray = mutableListOf<Int>()
+    var isBlock = true
+    var index = 0
+    input.readLines().first().forEach {
+        if (isBlock) {
+            (0 until it.digitToInt()).forEach { startingArray.add(index) }
+            isBlock = false
+        } else {
+            (0 until it.digitToInt()).forEach { startingArray.add(-1) }
+            isBlock = true
+            index++
+        }
     }
-    return TODO()
+    (startingArray.last() downTo 0).forEach { last ->
+        val size = startingArray.count { it == last }
+        var foundSoFar = 0
+        var index = 0
+        var done = false
+        startingArray.forEachIndexed { idx, i ->
+            if (!done) {
+                if (i != -1) {
+                    foundSoFar = 0
+                } else {
+                    if (foundSoFar == 0) index = idx
+                    foundSoFar++
+                    if (foundSoFar == size) {
+                        done = true
+                    }
+                }
+            }
+        }
+        if (done) {
+            (index until index + size).forEach { i ->
+                startingArray.removeAt(i)
+                startingArray.add(i, last)
+                val lastLast = startingArray.lastIndexOf(last)
+                startingArray.removeAt(lastLast)
+                startingArray.add(lastLast, -1)
+            }
+        }
+    }
+
+    return startingArray.mapIndexed { i, c ->
+        if (c == -1)
+            BigInteger.ZERO
+        else
+            i.toBigInteger() * c.toBigInteger()
+    }
+        .fold(BigInteger.ZERO) { acc, i ->
+            acc.plus(i)
+        }.toString()
 }
