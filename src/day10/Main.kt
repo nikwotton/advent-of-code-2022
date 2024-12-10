7d2864f532cdf4cfc016601fec23bbfe40db350e
Nikolai Wotton <wottonn@gmail.com>
2024-12-10 00:18:41 -0500

    day 10

--- src/day10/Main.kt
+++ src/day10/Main.kt
@@ -6,8 +6,8 @@ val workingDir = "src/${object {}.javaClass.`package`.name}"
 
 fun main() {
     val sample = File("$workingDir/sample.txt")
-    val step1SampleExpected = "TODO(step1)"
-    val step2SampleExpected = "TODO(step2)"
+    val step1SampleExpected = "36"
+    val step2SampleExpected = "81"
     val input1 = File("$workingDir/input_1.txt")
     val step1Sample = runStep1(sample)
     require(step1Sample == step1SampleExpected) { "Failed sample in step 1, got $step1Sample, instead of $step1SampleExpected" }
@@ -17,16 +17,54 @@ fun main() {
     println("Step 2 answer: ${runStep2(input1)}")
 }
 
+data class Position(
+    val x: Int,
+    val y: Int,
+    val height: Int,
+    var up: Position? = null,
+    var down: Position? = null,
+    var left: Position? = null,
+    var right: Position? = null
+)
+
 fun runStep1(input: File): String {
-    input.readLines().forEach {
-        TODO()
+    val map = input.readLines().mapIndexed { y, s -> s.mapIndexed { x, c -> Position(x, y, c.digitToInt()) } }
+    map.forEachIndexed { y, positions ->
+        positions.forEachIndexed { x, position ->
+            position.up = map.getOrNull(y - 1)?.getOrNull(x)
+            position.down = map.getOrNull(y + 1)?.getOrNull(x)
+            position.left = map.getOrNull(y)?.getOrNull(x - 1)
+            position.right = map.getOrNull(y)?.getOrNull(x + 1)
+        }
     }
-    return TODO()
+    fun helper(p: Position): List<Position> =
+        if (p.height == 9)
+            listOf(p)
+        else
+            listOfNotNull(p.up, p.down, p.left, p.right).filter { it.height == p.height + 1 }
+                .map { helper(it) }.flatten()
+    return map.flatten().filter { it.height == 0 }.map {
+        helper(it).map { "${it.x},${it.y}" }.toSet()
+    }.flatten().count().toString()
 }
 
 fun runStep2(input: File): String {
-    input.readLines().forEach {
-        TODO()
+    val map = input.readLines().mapIndexed { y, s -> s.mapIndexed { x, c -> Position(x, y, c.digitToInt()) } }
+    map.forEachIndexed { y, positions ->
+        positions.forEachIndexed { x, position ->
+            position.up = map.getOrNull(y - 1)?.getOrNull(x)
+            position.down = map.getOrNull(y + 1)?.getOrNull(x)
+            position.left = map.getOrNull(y)?.getOrNull(x - 1)
+            position.right = map.getOrNull(y)?.getOrNull(x + 1)
+        }
     }
-    return TODO()
+    fun helper(p: Position): List<Position> =
+        if (p.height == 9)
+            listOf(p)
+        else
+            listOfNotNull(p.up, p.down, p.left, p.right).filter { it.height == p.height + 1 }
+                .map { helper(it) }.flatten()
+    return map.flatten().filter { it.height == 0 }.map {
+        helper(it)
+    }.flatten().count().toString()
 }
---
+++ src/day11/Main.kt
@@ -0,0 +1,32 @@
+package day11
+
+import java.io.File
+
+val workingDir = "src/${object {}.javaClass.`package`.name}"
+
+fun main() {
+    val sample = File("$workingDir/sample.txt")
+    val step1SampleExpected = "TODO(step1)"
+    val step2SampleExpected = "TODO(step2)"
+    val input1 = File("$workingDir/input_1.txt")
+    val step1Sample = runStep1(sample)
+    require(step1Sample == step1SampleExpected) { "Failed sample in step 1, got $step1Sample, instead of $step1SampleExpected" }
+    println("Step 1 answer: ${runStep1(input1)}")
+    val step2Sample = runStep2(sample)
+    require(step2Sample == step2SampleExpected) { "Failed sample in step 2, got $step2Sample, instead of $step2SampleExpected" }
+    println("Step 2 answer: ${runStep2(input1)}")
+}
+
+fun runStep1(input: File): String {
+    input.readLines().forEach {
+        TODO()
+    }
+    return TODO()
+}
+
+fun runStep2(input: File): String {
+    input.readLines().forEach {
+        TODO()
+    }
+    return TODO()
+}
